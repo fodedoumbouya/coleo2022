4168438ba3ff779feb2d29d72fd61616496112f2
fodedoumbouya <doumbouyaf.fode@gmail.com>
2022-10-13 15:23:30 +0200

    creation de segmentation

---
+++ AI/main.py
@@ -0,0 +1,7 @@
+
+from segmentation import *
+import sys
+
+if __name__ == "__main__":
+    path = "./Utils/coleoi.jpg"
+    segmentation(sys.argv[1:], path,)
--- AI/segmentation.py
+++ AI/segmentation.py
@@ -1,13 +1,18 @@
-import sys
 import cv2 as cv
 import numpy as np
+# import json
+# import sys
+import json
 
 
-def main(argv):
+# Use cage
+# import sys et donner le nom du ficher
+## segmentation(sys.argv[1:], "coleoi.jpg")
 
-    default_file = 'coleoi.jpg'
+def segmentation(argv, path):
+    default_file = path
+    # 'coleoi.jpg'
     filename = argv[0] if len(argv) > 0 else default_file
-    # Loads an image
     src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
     # Check if image is loaded fine
     if src is None:
@@ -17,31 +22,34 @@ def main(argv):
         return -1
 
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-
     gray = cv.medianBlur(gray, 5)
-
     rows = gray.shape[0]
     circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 200,
                               param1=30, param2=30,
                               minRadius=1, maxRadius=30)
-
-    #  param 1 param 2 est le min des trou
-
+    mylist = []
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            center = (i[0], i[1])
-            # circle center
-            cv.circle(src, center, 1, (0, 100, 100), 3)
-            # circle outline
-            radius = i[2]
-            cv.circle(src, center, radius, (255, 0, 255), 3)
+            model = {
+                "coord": {
+                    "x": int(i[0]),
+                    "y": int(i[1]),
+                    "s": int(i[2])
+                },
+                "label": ""
+            }
+            mylist.append(model)
 
-    cv.imshow("detected circles", src)
-    cv.waitKey(0)
+    # removing the extenstion i
+    lastName = path.split("/")
+    nameWithExt = lastName[len(lastName)-1]
+    index = nameWithExt.split(".")
+    name = index[0]
+
+    p = "./data/"+name+".json"
+    # -creating the json
+    with open(p, "w",) as mon_fichier:
+        json.dump({path: mylist}, mon_fichier)
 
     return 0
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
