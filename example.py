68517ba5426949f845ba5084fbbb3f8199a7ffce
FILIPPI Jean-Baptiste <filippi@univ-corse.fr>
2022-10-27 16:13:11 +0200

    refactoring with examples

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 15:41:51 2022
+
+@author: filippi_j
+"""
+import src.coleolabels
+import src.affichage
+import src.segmentation
+import src.train
+
+
+trainingSetPath = "trainDatabase/e0/trainingSete0.json"
+
+#activationSetPath = "../Utils/trainingSet.json"
+#model = learnLabelledSet(json.load(open(trainingSetPath))
+#model.save('AI/trained_model.h5')
+
+
+#imagePathSet = json.load(open(trainingSetPath))
+
+#segmentedSet = segmentImageSet(imagePathSet)
+
+#classifiedSet = classifyImageSet(model,segmentedSet)
--- src/affichage.py
+++ src/affichage.py
@@ -5,27 +5,17 @@ import matplotlib.image as mpimg
 import matplotlib.patches as mpatches
 import json
 
+'''
 json_path = 'trainingSet.json'
-plt.rcParams['figure.dpi'] = 800
-labels = {"autres": [238/255,59/255,59/255],
-         "coleonica": [124/255,205/255,124/255],
-         "faux": [255/255,255/255,0/255],
-         "flat": [142/255,229/255,238/255],
-         "placodea": [255/255,211/255,155/255],
-         "pore": [0/255,201/255,87/255],
-         "ringed": [255/255,110/255,180/255],
-         "roundtip": [145/255,44/255,238/255],
-         "sting": [238/255,121/255,66/255],
-         "sunken": [0/255,0/255,255/255],
-         "wrinkled": [255/255,131/255,250/255],
-         "non classé": [0, 0, 0]}
 
-with open(json_path, 'r') as f:
-  data = json.load(f)
 
-#print(data)
 
-def show_image(image, output):
+#with open(json_path, 'r') as f:
+#  data = json.load(f)
+'''
+
+def show_image(imageKeySet, outfilename=None ):
+    plt.rcParams['figure.dpi'] = 800
     labels_count = {"autres": 0,
                     "coleonica": 0,
                     "faux": 0,
@@ -61,10 +51,11 @@ def show_image(image, output):
     img = mpimg.imread(image)
     plt.imshow(img,cmap='gray',vmin=0,vmax=255)
     lgd = plt.legend(handles=patches, loc='lower left', borderaxespad=0., bbox_to_anchor=(1.1, 0))
-    plt.savefig(output, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    if outfilename is not None :
+        plt.savefig(outfilename, bbox_extra_artists=(lgd,), bbox_inches='tight')
 
-name="output/output"
-i=0
-for image in data:
-    show_image(image, name+"_"+str(i))
-    i += 1
+#name="output/output"
+#i=0
+#for image in data:
+#    show_image(image, name+"_"+str(i))
+#    i += 1
