bd676b7e9dcd599f1f0c4661ac6aace50ce227f8
Edouard1er <edouardchevenslove98@gmail.com>
2022-12-07 16:57:13 +0200

    Add select class

---
+++ front/index.js
@@ -0,0 +1,64 @@
+var classJson = [
+    {
+        "classe":"coleonica",
+        "couleur":"#87CEEB"
+    },
+    {
+        "classe":"faux",
+        "couleur":"#B0E0E6"
+    },
+    {
+        "classe":"flat",
+        "couleur":"#B0C4DE"
+    },
+    {
+        "classe":"placodea",
+        "couleur":"#4682B4"
+    },
+    {
+        "classe":"pore",
+        "couleur":"#FFE4C4"
+    },
+    {
+        "classe":"ringed",
+        "couleur":"#e7feff"
+    },
+    {
+        "classe":"roundtip",
+        "couleur":"#F0E68C"
+    },
+    {
+        "classe":"sting",
+        "couleur":"#a1b19f"
+    },
+    {
+        "classe":"wrinkled",
+        "couleur":"#9ACD32"
+    },
+    {
+        "classe":"sunken",
+        "couleur":"#bcd4e6"
+    }
+]
+function generateClass(){
+    let classHtml=""
+    classJson.forEach(eachClass => {
+        classHtml +=`<div id="${eachClass.classe}" class="image-item" style="background-color: ${eachClass.couleur};">
+                        ${eachClass.classe}
+                    </div>`
+    })
+    $("#mouche-classe").html(classHtml)
+    let allClass=document.getElementsByClassName("image-item")
+    if(allClass && allClass.length > 0){
+        let allClassLength=allClass.length
+        for (let index = 0; index < allClassLength; index++) {
+            const element = allClass[index];
+            element.addEventListener("click", (e)=>{
+                $(".image-item").css("border-color","black")
+                allClass[index].style.borderColor  ="red"
+            })
+        }
+    }
+}
+
+generateClass()
